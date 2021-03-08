144999f865d022cb99ffccd9c3125d0202f58a8f
04D4BC2AE3759789CB2D61708E8CBCEE <04D4BC2AE3759789CB2D61708E8CBCEE@i-search.com.cn>
2021-03-08 17:02:41 +0800

    update

--- express-repository/src/main/java/com/szcinda/express/persistence/ProfileRepository.java
+++ express-repository/src/main/java/com/szcinda/express/persistence/ProfileRepository.java
@@ -2,10 +2,13 @@ package com.szcinda.express.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ProfileRepository extends JpaRepository<Profile, String>, JpaSpecificationExecutor<Profile> {
-    List<Profile> findByName(String name);
+    @Query(value = "select p from Profile p where p.name = :name or p.company = :name")
+    List<Profile> findByName(@Param("name") String name);
     Profile findFirstById(String id);
 }
